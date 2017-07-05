9b1267fbd886152064b9d56de858c6facbfc218d
dragon.mok <priclessmok@gmail.com>
2017-07-05 09:32:03 +0900

    세번째끄적거림

--- testfile.py
+++ testfile.py
@@ -3,3 +3,18 @@ print("""안녕하세요
 저는
 이용목 입니다.""")
 
+print("No new line", end="");print(" ok") #자동 줄바꿈 제
+
+try:
+    4/0
+except ZeroDivisionError as e:
+    print(e)
+
+a=2+3j
+b=3
+print(a*b)
+
+i=0
+while i<4:
+    i+=1
+    print(i)
