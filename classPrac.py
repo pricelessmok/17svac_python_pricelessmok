30a22ecea0db550438bcb568770a263954776102
dragon.mok <priclessmok@gmail.com>
2017-07-06 19:19:46 +0900

    클래스 학습중

---
+++ classPrac.py
@@ -0,0 +1,29 @@
+#1
+class Calc:
+    def __init__(self):
+        self.result=0
+    def adder(self, num):
+        self.result += num
+        return self.result
+
+calc1 = Calc()
+calc2 = Calc()
+
+print(calc1.adder(3))
+print(calc1.adder(4))
+print(calc2.adder(3))
+print(calc2.adder(7))
+
+#2
+class Service:
+    secret = "secret"
+    def setname(self, name):
+        self.name = name
+    def sum(self,a,b):
+        result = a+b
+        print("%s %d" %(self.name,result))
+
+svc = Service()
+svc.setname("lee")
+print(svc.secret)
+print(svc.sum(3,6))
